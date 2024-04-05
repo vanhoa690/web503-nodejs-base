65c6de23123f44f86017d18c3064f5e98e57e94f
Hoadv <68044489+vanhoa690@users.noreply.github.com>
2024-04-05 10:13:11 +0700

    code base nodemon

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from "express";
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Home");
 });
 
-export const viteNodeApp = app;
+app.listen(3000, () => {
+  console.log(`Server on port 3000`);
+});
